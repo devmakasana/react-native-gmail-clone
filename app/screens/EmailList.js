dbc94f0fc9d92a97401344336d8555f886e93bf5
Harikrushna-pixer <harikrushna@pixer.io>
2019-12-31 18:49:26 +0530

    EmailList Screen
    - desing flatlist mail (row)

--- app/screens/EmailList.js
+++ app/screens/EmailList.js
@@ -18,6 +18,7 @@ class emailList extends Component {
   render() {
     return (
       <SafeAreaView style={[AppStyles.root, AppStyles.rootHorizontalPad]}>
+        {/* Searchbar */}
         <View style={s.shadowBox}>
           <View style={s.headerSearch}>
             <SvgIcon svgs={svgs} name={'short-menu'} width={30} height={20} />
@@ -26,6 +27,113 @@ class emailList extends Component {
               placeholder={'Search mail'}
               containerStyle={s.inputWrap}></Input>
             <AppAvtar></AppAvtar>
+          </View>
+        </View>
+        {/* Email-Listing */}
+        <View style={s.mailListing}>
+          <View style={styles.container}>
+            <Text style={styles.primaryText}>PRIMARY</Text>
+            <View style={styles.Box}>
+              <View style={styles.leftWrap}>
+                <SvgIcon
+                  style={styles.exportImgWrapper}
+                  svgs={svgs}
+                  name={'author-icon'}
+                  fill={'none'}
+                  width={42}
+                  height={42}
+                />
+                <View style={styles.caption}>
+                  <Text style={styles.BookText}>BookMyShow</Text>
+                  <Text style={styles.Book}>
+                    Prepare for the food binge of your lif...
+                  </Text>
+                  <Text style={styles.Bill}>
+                    Book for the Ahmedabad Food Fest...
+                  </Text>
+                </View>
+              </View>
+              <View style={styles.rightWrap}>
+                <Text style={styles.day}>26 Dec</Text>
+                <SvgIcon
+                  style={styles.exportImgWrapper}
+                  svgs={svgs}
+                  name={'vector-icon'}
+                  fill={'none'}
+                  width={28}
+                  height={28}
+                />
+              </View>
+            </View>
+
+            {/* tow */}
+            <View style={styles.Tow}>
+              <View style={styles.Box}>
+                <View style={styles.leftWrap}>
+                  <SvgIcon
+                    style={styles.exportImgWrapper}
+                    svgs={svgs}
+                    name={'author-icon'}
+                    fill={'none'}
+                    width={42}
+                    height={42}
+                  />
+                  <View style={styles.caption}>
+                    <Text style={styles.BookText}>BookMyShow</Text>
+                    <Text style={styles.Book}>
+                      Prepare for the food binge of your lif...
+                    </Text>
+                    <Text style={styles.Bill}>
+                      Book for the Ahmedabad Food Fest...
+                    </Text>
+                  </View>
+                </View>
+                <View style={styles.rightWrap}>
+                  <Text style={styles.day}>26 Dec</Text>
+                  <SvgIcon
+                    style={styles.exportImgWrapper}
+                    svgs={svgs}
+                    name={'vector-icon'}
+                    fill={'none'}
+                    width={28}
+                    height={28}
+                  />
+                </View>
+              </View>
+            </View>
+            {/* threee */}
+            <View style={styles.Box}>
+              <View style={styles.leftWrap}>
+                <SvgIcon
+                  style={styles.exportImgWrapper}
+                  svgs={svgs}
+                  name={'author-icon'}
+                  fill={'none'}
+                  width={42}
+                  height={42}
+                />
+                <View style={styles.caption}>
+                  <Text style={styles.BookText}>BookMyShow</Text>
+                  <Text style={styles.Book}>
+                    Prepare for the food binge of your lif...
+                  </Text>
+                  <Text style={styles.Bill}>
+                    Book for the Ahmedabad Food Fest...
+                  </Text>
+                </View>
+              </View>
+              <View style={styles.rightWrap}>
+                <Text style={styles.day}>26 Dec</Text>
+                <SvgIcon
+                  style={styles.exportImgWrapper}
+                  svgs={svgs}
+                  name={'vector-icon'}
+                  fill={'none'}
+                  width={28}
+                  height={28}
+                />
+              </View>
+            </View>
           </View>
         </View>
       </SafeAreaView>
@@ -57,6 +165,62 @@ const s = StyleSheet.create({
     marginLeft: scale(lessIndent),
     marginRight: scale(lessIndent),
     paddingHorizontal: scaleVertical(0),
+  },
+  // Email LIsting
+  container: {
+    padding: 16,
+  },
+  primaryText: {
+    color: colors.secondary,
+    ...Typography.subTitle,
+    marginBottom: scaleVertical(21),
+  },
+  caption: {
+    paddingHorizontal: 18,
+  },
+  Book: {
+    ...Typography.caption,
+    color: colors.primary,
+    letterSpacing: 0.3,
+    fontWeight: fontWeights.bold,
+  },
+  Box: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'space-between',
+  },
+  leftWrap: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  BookText: {
+    ...Typography.label,
+    color: colors.primary,
+    fontSize: fontSizes.lable,
+    fontWeight: fontWeights.bold,
+    letterSpacing: 0.4,
+  },
+  Bill: {
+    fontSize: 14,
+    lineHeight: 16,
+    fontFamily: 'Roboto',
+    letterSpacing: 0.4,
+    color: colors.secondary,
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+  },
+  rightWrap: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  day: {
+    ...Typography.span,
+    color: colors.black,
+    fontWeight: fontWeights.bold,
+    fontStyle: 'normal',
+    alignItems: 'center',
+    flex: 1,
+    marginTop: scaleVertical(4),
   },
 });
 
